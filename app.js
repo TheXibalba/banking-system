1eb9ae487b91d1f2ac82bd3a54cb9f530b5cf095
Yuvraj Pawar <yurajpawar99@gmail.com>
2021-08-16 00:11:07 +0530

    Changed the port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const ejs = require("ejs");
 const cors = require("cors");
 const { indexController } = require("./controllers/indexController");
