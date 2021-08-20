6928530389655455b1a93b27afe3e7023c6c1b6a
Yuvraj Pawar <yurajpawar99@gmail.com>
2021-08-20 22:10:27 +0530

    Sorted the customer list

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,18 +1,16 @@
 const Customer = require("../models/customerModel");
 
 exports.indexController = (req, res) => {
-  Customer.find({}, (err, customers) => {
-    res.render("index", {
-      customers,
- 
+  Customer.find()
+    .sort("name")
+    .exec((err, customers) => {
+      if(!err){
+
+        res.render("index", {
+          customers,
+        });
+      }else{
+        res.status(500);
+      }
     });
-  });
 };
-
-
-
-
-
-
-
-
